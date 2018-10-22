6836ed43b988402f6c42bfd079ac5705e0ca0481
Marc <marc.troughton@gmail.com>
2018-10-22 22:42:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,14 +6,17 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return 'Happy Holidays, ${name}!';
+  return `Happy Holidays, ${name}!`;
 }
 
 function happyHolidayTo(holiday, name) {
-  return 'Happy ${holiday}, ${name}!';
+  return 'Happy ${holiday}, ${name}!'
 }
 
 function holidayCountdown(holiday, days) {
-  return "It's ${days} days until ${holiday}!";
+  return "It's ${days} days until ${holiday}!"
 }
 
+function say(greeting, firstName) {
+  return `${greeting}, ${firstName}!`
+}
